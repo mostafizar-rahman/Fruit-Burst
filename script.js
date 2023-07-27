e3d29e7966d15b3f47a337641136bdbe17b7cbaa
Mostafizur <mostafizurdh2000@gmail.com>
2023-07-27 18:54:57 +0600

    bug fix 1

---
+++ script.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    $('.banner').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: $(".pp2"),
+        nextArrow: $(".nn2"),
+      });
+});
